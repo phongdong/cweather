246b7d1600f0ec4f301da26efa16f25e9b2a5494
Phong Dong <phongdongt@gmail.com>
2017-10-29 09:25:05 -0700

    first commit

---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class WeatherProvider {
+  apiKey = 'fbdc4a535551215e';
+  url;
+
+  constructor(public http: Http) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
+  }
+
+  getWeather(city, state) {
+    return this.http.get(this.url+'/'+state+'/'+city+'.json')
+                .map(res => res.json());
+  }
+
+}
